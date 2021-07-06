e4b603986a4148d98ab2cdada37336ad779e4b9e
Vikas Jonwal <58461513+VikasJonwal@users.noreply.github.com>
2021-07-06 14:21:54 +0530

    day 7 completed

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_demo/pages/home_page.dart';
 import 'package:flutter_demo/pages/login_page.dart';
 import 'package:flutter_demo/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(MyApp());
